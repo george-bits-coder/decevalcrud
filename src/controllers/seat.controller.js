eb68530d8895643fa8176b68c9a21009c8aa7055
george-bits-coder <91771684+george-bits-coder@users.noreply.github.com>
2021-12-13 16:50:37 +0530

    Update seat.controller.js

--- src/controllers/seat.controller.js
+++ src/controllers/seat.controller.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const Seat = require("../models/seat.model");
 
-const { postOne, getAll, getOne, updateOne, deleteOne } = require("./crud.controller");
+const { postOne, getAll, getOne, updateOne } = require("./crud.controller");
 
 const router = express.Router();
 
@@ -11,7 +11,7 @@ router.post("/", postOne(Seat));
 router.get("/", getAll(Seat));
 router.get("/:id", getOne(Seat));
 router.patch("/:id", updateOne(Seat));
-router.delete("/:id", deleteOne(Seat));
+
 
 
 module.exports = router;

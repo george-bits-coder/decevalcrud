27d7434d0e2a943b1556f2379af3587991a49507
george-bits-coder <91771684+george-bits-coder@users.noreply.github.com>
2021-12-13 16:35:24 +0530

    Add files via upload

---
+++ src/models/show.model.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require("mongoose");
+
+const showSchema = new Schema({
+    timing: { type: String, required: true },
+    total_seats: { type: Number, required: true },
+    movie_id: {
+        type: Schema.Types.ObjectId,
+        ref: "movie",
+        required: true
+    },
+    screen_id: {
+        type: Schema.Types.ObjectId,
+        ref: "screen",
+        required: true
+    },
+}, {
+    versionKey: false,
+    timestamps: true,
+});
+
+module.exports = model("show", showSchema);
